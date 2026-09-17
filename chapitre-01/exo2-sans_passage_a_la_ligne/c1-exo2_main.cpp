bb3fff45ebbe108e8f3393b7d6838122a7c05d9a
lucynakirie454-spec <lucynakirie454@gmail.com>
2026-09-17 15:31:20 +0100

    Update c1-exo2_main.cpp

--- chapitre-01/exo2-sans_passage_a_la_ligne/c1-exo2_main.cpp
+++ chapitre-01/exo2-sans_passage_a_la_ligne/c1-exo2_main.cpp
@@ -3,8 +3,5 @@ int main()
 {
 printf("MENGUE KERINE");
 printf("yaounde");
-
-//lorsqu'on enleve aller a la ligne les deux phrases ne sont plus affiches sur deux lignes differentes.ils apparaissent sur une meme ligne,l'un a la suite de l'autre.
-//cela ne fait pas un bug car on n'a pas demander a la machine d'aller a la ligne.donc l'ordinateur execute juste ce qui lui a ete demander.
         return 0;
 }
