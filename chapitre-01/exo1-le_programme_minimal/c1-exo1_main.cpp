cc9d8277bad74f9db7334ffdc342969d066b44a6
lucynakirie454-spec <lucynakirie454@gmail.com>
2026-09-17 05:08:56 +0100

    ajout  du programme double

---
+++ chapitre-01/exo1-le_programme_minimal/c1-exo1_main.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+int main()
+{
+    char nom[30];
+     char ville[25];
+printf("entrer votre nom : ");
+ scanf("%s", nom);
+  printf("votre nom est %s\n" nom);
+
+  printf("entrer le nom de votre ville :"  );
+     scanf("%s", ville);
+       printf("le nom de votre ville est %S\n" ville);
+        return 0;
+}
