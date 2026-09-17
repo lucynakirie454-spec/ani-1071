2f56bc66f116abef7e99d3e96a245f6c57bc4028
lucynakirie454-spec <lucynakirie454@gmail.com>
2026-09-17 03:40:48 +0100

    Ajout du programme double

---
+++ exo1-le_programme_minimal/c1-exo1_main.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+int main(){
+     
+    int nombre;
+    printf("entrer un nombre : ");
+     scanf("%d",&nombre);
+     printf("le double est: %d\n",nombre*2);
+
+      return 0;
+
+}
