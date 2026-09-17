71e47715d265600d1c20970cec04dde3ef2220f6
lucynakirie454-spec <lucynakirie454@gmail.com>
2026-09-17 12:07:28 +0100

    remplacer le code par le nouveau code

--- chapitre-01/exo1-le_programme_minimal/c1-exo1_main.cpp
+++ chapitre-01/exo1-le_programme_minimal/c1-exo1_main.cpp
@@ -2,13 +2,8 @@
 int main()
 {
     char nom[30];
-     char ville[30];
-printf("entrer votre nom :" );
- scanf("%29s", nom);
-  printf("votre nom est %s\n", nom);
-
-  printf("entrer le nom de votre ville :"  );
-     scanf("%29s", ville);
-       printf("le nom de votre ville est %s\n", ville);
-        return 0;
+    char ville[30];
+    printf("MENGUE KERINE \n ");
+    printf("yaounde \n");
+     return 0 ;
 }
