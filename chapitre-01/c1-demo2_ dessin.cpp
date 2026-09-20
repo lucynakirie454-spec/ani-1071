63fe268b6bd729e1ef4848e49b928796a52aef2d
lucynakirie454-spec <lucynakirie454@gmail.com>
2026-09-20 20:58:41 +0100

    Add files via upload

---
+++ chapitre-01/c1-demo2_ dessin.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+int main(){
+
+    printf("####################\n");
+    printf("#                  #\n");
+    printf("#                  #\n");
+    printf("#                  #\n");   
+    printf("#                  #\n");          
+    printf("#                  #\n");
+    printf("####################\n");
+    return 0;
+}
