946fabda250940210e1c8f81c92b092d6e9f9b36
lucynakirie454-spec <lucynakirie454@gmail.com>
2026-09-25 15:32:02 +0100

    Add files via upload

---
+++ chapitre-02/c2-exo8_main.cpp
@@ -0,0 +1,14 @@
+
+#include <cstdio>
+ int main(){
+
+    int a =3;
+    int b;
+    int c ;
+     b = a++ + 1;
+     printf("b = %d ,a, = %d\n" ,b,a );
+      c = ++a * 2;
+       printf("c = %d, a = %d\n",c,a);
+         return 0;
+
+ }
